d0a75a0abd0a920dc0d1b9b272964de5d9a364ac
Taylor Waddel <taylor.waddel@gmail.com>
2020-05-13 08:52:53 +1200

    Initial Commit

---
+++ SimpleYahtzee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleYahtzee
+//
+//  Created by Taylor Waddel on 13/05/20.
+//  Copyright © 2020 Taylor Waddel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
