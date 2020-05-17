47a2b0520c2d155e972a2e465e9aa13fb621b9d5
Taylor Waddel <taylor.waddel@gmail.com>
2020-05-17 21:34:47 +1200

    Started creating the triple yahtzee game mode.

--- SimpleYahtzee/Views/TripleGameViewController.swift
+++ SimpleYahtzee/Views/TripleGameViewController.swift
@@ -2,12 +2,19 @@
 //  TripleGameViewController.swift
 //  SimpleYahtzee
 //
-//  Created by Taylor Waddel on 14/05/20.
+//  Created by Taylor Waddel on 17/05/20.
 //  Copyright © 2020 Taylor Waddel. All rights reserved.
 //
 
 import UIKit
 
 class TripleGameViewController: UIViewController {
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Change the back button to say "Quit" instead
+        let backButton = UIBarButtonItem()
+        backButton.title = "Quit"
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
+    }
 }
